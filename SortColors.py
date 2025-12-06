e3e3eaccdad4e8395706784405194ae20d3cadd9
jabohl <94003824+jabohl@users.noreply.github.com>
2025-12-06 18:44:31 -0500

    Update SortColors.py

--- SortColors.py
+++ SortColors.py
@@ -3,22 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        buckets=[[],[],[]]
-        start=0
-        mid=start+1
-        end=len(nums)-1
-        test=nums
-        for i  in range(len(test)):
-            if test[i] ==0:
-                buckets[0].append(0)
-            if test[i] ==1:
-                buckets[1].append(1)
+    
+        if len(nums) < 2:
+            return
+        index = 0
+        startPtr = 0
+        endPtr = len(nums)-1
+        while(index <= endPtr):
+            if nums[index] == 0:
                 
-            if test[i] ==2:
-                buckets[2].append(2)
-        bucket=0
-        start=0
-        for bucket in range(3):
-            nums[start:start+len(buckets[bucket])]=buckets[bucket]
-            start+=len(buckets[bucket])
-        print(nums)
+                temp = nums[startPtr]
+                nums[startPtr] = nums[index]
+                nums[index] = temp
+                index += 1
+                startPtr += 1
+            elif nums[index] == 1:
+                index += 1
+            else:
+                temp = nums[endPtr]
+                nums[endPtr] = nums[index]
+                nums[index] = temp
+                endPtr -= 1
