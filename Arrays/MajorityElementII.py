56a11cc637ab1b03f96660077206c37b97e8595b
James Bohl <94003824+jabohl@users.noreply.github.com>
2026-04-08 20:52:42 -0400

    Update MajorityElementII.py

--- Arrays/MajorityElementII.py
+++ Arrays/MajorityElementII.py
@@ -1,12 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        myDict={}
-        for i in nums:
-            myDict[i]=[]
-        for i in range(len(nums)):
-            myDict[nums[i]].append(i)
-        lst=[]
+        elementDictionary={}
+        for index in nums:
+            elementDictionary[index] = []
+            
+        for index in range(len(nums)):
+            elementDictionary[nums[index]].append(index)
+        
+        listOfElements=[]
+        
         for val in myDict:
-            if len(myDict[val])>len(nums)/3:
-                lst.append(val)
-        return lst
+        
+            if len(elementDictionary[val]) > len(nums) / 3:
+            
+                listOfElements.append(val)
+        
+        return listOfElements
